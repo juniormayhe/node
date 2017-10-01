a677a743923a8f1a4ad079ed859ea66599a0432b
Wanderley Mayhe Junior <juniormayhe@gmail.com>
2017-10-01 14:49:17 -0500

    Add tests for DELETE /users/me/token

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -310,3 +310,24 @@ describe('POST /users/login', ()=>{
         });
     });
 });
+
+//test delete user token
+describe('DELETE /users/me/token', ()=> {
+    it('should remove auth token on logout', (done)=>{
+        //supertest
+        request(app)
+        .delete('/users/me/token')
+        .set('x-auth', users[0].tokens[0].token)
+        .expect(200)
+        .end((err,res)=>{
+            if (err){
+                return done(err);
+            }
+            User.findById(users[0]._id).then(user=>{
+                //check if token array is length zero
+                expect(user.tokens.length).toBe(0);
+                done();
+            }).catch(e=> done(e));
+        });
+    });
+});

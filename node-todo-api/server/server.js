10e83ad4129fa5aca31e913cb0df99566d2e41ed
Wanderley Mayhe Junior <juniormayhe@gmail.com>
2017-10-07 10:26:30 -0500

    debugging

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -138,7 +138,7 @@ app.post('/users', (req, res)=>{
         res.header('x-auth', token).send(user);
     })
     .catch((err)=> {
-        console.log(ex, ex.stack.split("\n"));
+        console.log(err, err.stack.split("\n"));
         res.status(400).send(err);
     });
     //console.log(req.body);
